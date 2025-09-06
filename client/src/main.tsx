74d3a237aed4c732e0df43d773000de969de16c1
Paulius <p.jacikeviciusinfo@gmail.com>
2025-09-06 22:45:55 +0300

    refactor(main.tsx): add future providers

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import { SearchProvider } from './contexts/SearchContext.tsx';
+import { PackageProvider } from './contexts/PackageContext.tsx';
+import { BrowserRouter } from 'react-router';
+import { createRoot } from 'react-dom/client';
 import App from './App.tsx'
+import './index.css'
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+ <BrowserRouter>
+    <PackageProvider>
+      <SearchProvider>
+        <App />
+      </SearchProvider>
+    </PackageProvider>
+  </BrowserRouter>
 )
