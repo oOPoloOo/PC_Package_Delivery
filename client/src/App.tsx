092e9cf1532551ee768903ef88b0f422db6c9c2a
Paulius <p.jacikeviciusinfo@gmail.com>
2025-09-07 15:25:22 +0300

    refactor(App.tsx): add new package route

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,18 +1,16 @@
 import { Routes, Route } from "react-router";
 import MainOutlet from "./components/outlets/MainOutlet";
 import Home from "./components/pages/HomePage";
+import NewPackagePage from "./components/pages/NewPAckagePage";
 // import PackageDetails from "./components/pages/PackageDetails";
-// import Creation from "./components/pages/Creation";
-
 
 function App() {
-
   return ( 
      <Routes> 
       <Route path="" element={<MainOutlet />}>
         <Route index element={<Home />} />
-        {/* <Route path="package/:id" element={<PackageDetails />}/>
-        <Route path="addPackage" element={<Creation />}/>     */}
+        {/* <Route path="package/:id" element={<PackageDetails />}/> */}
+        <Route path="addNewPackage" element={<NewPackagePage />}/>    
       </Route>
     </Routes>    
   )
