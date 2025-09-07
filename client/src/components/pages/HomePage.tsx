689822dc4fa1c0553005af067bb4f1624f811fef
Paulius <p.jacikeviciusinfo@gmail.com>
2025-09-07 11:59:40 +0300

    refactor(ui): make home page bones to show data

--- client/src/components/pages/HomePage.tsx
+++ client/src/components/pages/HomePage.tsx
@@ -1,18 +1,48 @@
-import React from 'react';
+import { useContext } from "react";
+import styled from "styled-components";
+// import Card from "../UI/molecules/PackageCard";
+import PackageContext from "../../contexts/PackageContext";
+import type { PackageContextType  } from "../../types";
 
-const HomePage: React.FC = () => {
-    return (
-        <div
-            style={{
-                minHeight: '100vh',
-                display: 'flex',
-                alignItems: 'center',
-                justifyContent: 'center',
-            }}
-        >
-            <h1>To be implemented</h1>
-        </div>
-    );
+const Content = styled.div`
+  display: flex;
+  flex-direction: column;
+  background: linear-gradient(180deg, #0f2027, #203a43, #2c5364);
+  color: white;
+  padding: 20px;
+  flex-grow: 1;
+
+  h1 {
+    margin-top: 10px;
+  }
+`;
+
+const StyledPostsContainer = styled.div`
+  display: flex;
+  gap: 20px;
+  justify-content: space-between;
+  flex-wrap: wrap;
+`;
+
+const HomePage = () => {
+  const { packages } = useContext(PackageContext) as PackageContextType;
+
+  return (
+    <Content>  
+
+      <h1>Deliver everything, everywhere!</h1>
+
+      <StyledPostsContainer>
+        {packages.length ? (
+        //   packages.map((item) => <Card key={item.id} data={item} />)
+        <p>We have packages to show working on it!</p>
+        ) : (
+          <p>Loading...</p>
+        )}
+      </StyledPostsContainer>
+
+    </Content>
+  );
 };
 
 export default HomePage;
