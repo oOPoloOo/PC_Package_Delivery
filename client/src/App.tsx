a51b24a1f48f09b2d9fea0626cf960bde1433885
Paulius <p.jacikeviciusinfo@gmail.com>
2025-09-07 16:22:55 +0300

    fix(ui): change  import name

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,7 @@
 import { Routes, Route } from "react-router";
 import MainOutlet from "./components/outlets/MainOutlet";
 import Home from "./components/pages/HomePage";
-import NewPackagePage from "./components/pages/NewPAckagePage";
+import NewPackagePage from "./components/pages/NewPackagePage";
 // import PackageDetails from "./components/pages/PackageDetails";
 
 function App() {
