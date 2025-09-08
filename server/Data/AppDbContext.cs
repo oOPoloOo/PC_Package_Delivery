2aa37b7d1599d1ee906a34c3831e9f2eee60c47c
Paulius <p.jacikeviciusinfo@gmail.com>
2025-09-08 13:28:50 +0300

    refactor(data): update package and status entities in db

--- server/Data/AppDbContext.cs
+++ server/Data/AppDbContext.cs
@@ -7,15 +7,25 @@ public class AppDbContext : DbContext
 {
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }   
 
-    public DbSet<PackageEntity> Packages {get; set; } = null!;
+    public DbSet<PackageEntity> Packages => Set<PackageEntity>();
+    public DbSet<StatusChange> StatusChanges => Set<StatusChange>();
+
 
     //Map model to the database 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
-        base.OnModelCreating(modelBuilder);
+        modelBuilder.Entity<PackageEntity>(b =>
+        {
+            b.HasKey(p => p.Id);
+            b.OwnsOne(p => p.Sender);
+            b.OwnsOne(p => p.Recipient);
+            b.HasMany(p => p.History).WithOne().HasForeignKey(h => h.PackageId);
+        });
 
-        modelBuilder.Entity<PackageEntity>()
-            .Property(p => p.CurrentStatus)
-            .HasConversion<string>();
+        modelBuilder.Entity<StatusChange>(b =>
+        {
+            b.HasKey(s => s.Id);
+            b.Property(s => s.Status).HasConversion<string>();
+        });
     }
 }
